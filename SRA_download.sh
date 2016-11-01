ad52c64f02ad0850063eef2a82c059f85c138beb
liux1299 <liux1299@umn.edu>
2016-11-01 14:57:12 -0500

    Added comments and moved hardcoded lines to beginning of script

--- SRA_download.sh
+++ SRA_download.sh
@@ -5,29 +5,30 @@
 #PBS -M mfrodrig@umn.edu 
 #PBS -q lab
 
-# Collect file from command line
+#   Filepaths provided by user:
+#   Location of SRA download script, from Tom Kono's Misc_Utils
+SRA_FETCH=${HOME}/software/tkono/Misc_Utils/SRA_Fetch.sh
+#   Directory where SRA files will be downloaded
+OUTPUT=/scratch/fernanda/baseline
+#   Collect file from command line
 SRA_FILES=${HOME}/baseline/SRR_Acc_List-2.txt
 
-# Make sure the file exists
+#   Make sure the file exists
 if ! [[ -f "${SRA_FILES}" ]]
     then echo "Failed to find ${SRA_FILES}, exiting..." >&2
     exit 1
     fi 
 
-# Make the array using command substitution
+#   Make the array using command substitution
 declare -a SRA_ARRAY=($(cat "${SRA_FILES}")) 
 
-#Print the values of the array to screen
+#   Print the values of the array to screen
 printf '%s\n' "${SRA_ARRAY[@]}"
 
-# location of SRA download script, from Tom Kono's Misc_Utils
-SRA_FETCH=${HOME}/software/tkono/Misc_Utils/SRA_Fetch.sh
-
-# directory where SRA files will be downloaded
-OUTPUT=/scratch/fernanda/baseline
-
-# iterate over every each of the run numbers in a lit of SRA files
-# and download to specified directory
+#   Iterate over every each of the run numbers in a lit of SRA files
+#   and download to specified directory
 for i in "${SRA_ARRAY[@]}"
+    #   -r option means provided accession is a run number
+    #   -d option outputs all SRA files into OUTPUT
     do bash $SRA_FETCH -r $i -d $OUTPUT
     done
